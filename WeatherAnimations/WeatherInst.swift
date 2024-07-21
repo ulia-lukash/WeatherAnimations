a28ebeebf85e3bfac80ea7b81f08af3684d0dce1
Ulia Lukash <ulianalukash@yandex.ru>
2024-07-21 18:11:39 +0300

    feat data

---
+++ WeatherAnimations/WeatherInst.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherInst.swift
+//  WeatherAnimations
+//
+//  Created by Uliana Lukash on 21.07.2024.
+//
+
+import Foundation
+
+struct WeatherInst {
+    let title: String
+    let imageName: String
+}
---
+++ WeatherAnimations/consts.swift
@@ -0,0 +1,15 @@
+//
+//  consts.swift
+//  WeatherAnimations
+//
+//  Created by Uliana Lukash on 21.07.2024.
+//
+
+import Foundation
+
+let WEATHER: [WeatherInst] = [
+    WeatherInst(title: "Ясно", imageName: "sun.max.fill"),
+    WeatherInst(title: "Дождь", imageName: "cloud.rain.fill"),
+    WeatherInst(title: "Гроза", imageName: "cloud.bolt.fill"),
+    WeatherInst(title: "Туман", imageName: "cloud.fog.fill")
+]
